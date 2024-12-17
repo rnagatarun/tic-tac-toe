f501b9c9df676ebb902a42b2b7f7ad3f6389972e
NagaTarun <rnagatarun@gmail.com>
2024-12-17 19:33:14 +0530

    update gameboard and package files

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 interface GameboardProps {
   onSelectSquare: (rowIndex: number, colIndex: number) => void;
   board: (string | null)[][];
@@ -7,18 +5,6 @@ interface GameboardProps {
 
 export default function Gameboard({ onSelectSquare, board }: GameboardProps) {
   const gameBoard = board;
-  // const[gameBoard,setGameBoard]=useState(initialGameboard);
-
-  // function handleSelectSquare(rowIndex: number, colIndex: number) {
-  //     setGameBoard((prevGameBoard) => {
-  //         const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-  //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //         return updatedGameBoard;
-  //         }
-  //     );
-  //     onSelectSquare();
-  // }
-
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
